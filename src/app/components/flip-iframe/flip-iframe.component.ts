e993c83ebeb00c991306b3c28f751fd498bfca93
INTEREDLB\ayaacoub <ayadjacob7@gmail.com>
2023-12-01 15:05:10 +0200

    Update-09

--- src/app/components/flip-iframe/flip-iframe.component.ts
+++ src/app/components/flip-iframe/flip-iframe.component.ts
@@ -1,11 +1,18 @@
 // flip-iframe.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-flip-iframe',
   templateUrl: './flip-iframe.component.html',
   styleUrls: ['./flip-iframe.component.scss'],
 })
-export class FlipIframeComponent  {
- 
+export class FlipIframeComponent {
+  projectUrl: SafeResourceUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/FlipProjects/computing_level_c/index.html');
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  changeProjectUrl(url: string) {
+    this.projectUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
