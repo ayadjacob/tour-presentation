7d1d4a56f7f7a2bc43f97b81d2aca92d8616842a
INTEREDLB\ayaacoub <ayadjacob7@gmail.com>
2023-12-01 18:33:59 +0200

    Update-13

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import screenfull from 'screenfull';
 
@@ -7,10 +7,26 @@ import screenfull from 'screenfull';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   isFullScreenActive: boolean = false;
+  isMusicPlaying: boolean = true;
+  audio: HTMLAudioElement = new Audio(); // Declare class-level Audio object
 
   constructor(private router: Router) {}
+
+  @HostListener('document:fullscreenchange', ['$event'])
+  onFullscreenChange(event: Event): void {
+    this.isFullScreenActive = screenfull.isFullscreen;
+  }
+
+  ngOnInit(): void {
+    this.playMusic(); // Play music on component initialization
+  }
+
+  playMusic(): void {
+    this.audio.src = 'assets/Music/Music.mp3'; // Replace with the actual path to your MP3 file
+    this.audio.play();
+  }
 
   isHistoricalPage(): boolean {
     const currentUrl = this.router.url;
@@ -29,6 +45,30 @@ export class FooterComponent {
       }
       this.isFullScreenActive = !this.isFullScreenActive;
     }
+  }
+
+  toggleMusic(): void {
+    // Add logic to play or stop the music here
+    this.isMusicPlaying = !this.isMusicPlaying;
+
+    // You can add your music playing logic here based on this.isMusicPlaying
+    if (this.isMusicPlaying) {
+      this.playMusic();
+    } else {
+      this.audio.pause();
+      this.audio.currentTime = 0;
+      // Play music when toggling it on
+    }
+  }
+
+  getMusicImage(): string {
+    return this.isMusicPlaying && this.isHistoricalPage()
+      ? "url('../../../assets/Images/Music_b.png')"
+      : this.isMusicPlaying
+      ? "url('../../../assets/Images/Music.png')"
+      : this.isHistoricalPage()
+      ? "url('../../../assets/Images/Mute_b.png')"
+      : "url('../../../assets/Images/Mute.png')";
   }
 
   getFullScreenImage(): string {
