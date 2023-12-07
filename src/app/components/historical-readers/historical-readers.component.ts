02f4182e26fd76d22cd2b6e340a2cc569d0e04da
INTEREDLB\ayaacoub <ayadjacob7@gmail.com>
2023-12-07 15:52:55 +0200

    Update-24

--- src/app/components/historical-readers/historical-readers.component.ts
+++ src/app/components/historical-readers/historical-readers.component.ts
@@ -1,38 +1,61 @@
-import { AfterViewInit, Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-historical-readers',
   templateUrl: './historical-readers.component.html',
   styleUrls: ['./historical-readers.component.scss'],
 })
-export class HistoricalReadersComponent implements AfterViewInit {
+export class HistoricalReadersComponent {
+  book1:any;
+  // initialBookWidth;
+  // initialBookHeight;
+  // initialWindowWidth;
 
-  constructor(private renderer: Renderer2, private el: ElementRef) {}
-
-  ngAfterViewInit() {
-    this.onViewportChange(); // Call the function initially
-
-    // Optional: You can use this to trigger the function when the component is initially created
-    // setTimeout(() => this.onViewportChange());
+  constructor() {
+    // this.initialBookWidth = 100;
+    // this.initialBookHeight = 115;
+    // this.initialWindowWidth = window.innerWidth;
   }
 
-  @HostListener('window:resize', ['$event'])
-  onViewportChange(event?: Event) {
-    // Get the element by class name
-    const coverContainer = this.el.nativeElement.querySelector('.cover-container');
+  // ngAfterViewInit() {
+  //   this.book1 = document.querySelector('#book-1');
+  //   this.initialBookWidth = this.book1?.offsetWidth ?? 0;
+  //   this.initialBookHeight = this.book1?.offsetHeight ?? 0;
+  //   this.initialWindowWidth = window.innerWidth;
+  //   this.onViewportChange(); // Call the function initially
+  // }
 
-    // Check if the element is found
-    if (coverContainer) {
-      // Get the width and height of the div
-      const width = coverContainer.offsetWidth;
-      const height = coverContainer.offsetHeight;
+  // @HostListener('window:resize', ['$event'])
+  // onViewportChange() {
+  //   var imageContainer:any= document.querySelector('.image-container');
 
-      // Show the width and height (you can replace this with any logic you need)
-      console.log('Width:', width, 'Height:', height);
+  //   var containerWidth = imageContainer.offsetWidth;
+  //   var containerHeight = imageContainer.offsetHeight;
 
-      // Alternatively, you can update the content of a property in your component
-      // For example, if you have a property named 'dimensions'
-      // this.dimensions = { width, height };
-    }
-  }
+  //   // Assuming the initial top and left percentages were 70% and 30%
+  //   var newTop = containerHeight * 0.7;
+  //   var newLeft = containerWidth * 0.3;
+
+  //   // Hone lezem thadel resize bass afdal enak ta3mela media query
+  //   var newWidth = this.initialBookWidth;
+  //   var newHeight = this.initialBookHeight;
+  //   if (window.innerWidth < this.initialWindowWidth) {
+  //     newWidth -= 10;
+  //     newHeight -= 10;
+  //   } else if (window.innerWidth > this.initialWindowWidth) {
+  //     newWidth += 10;
+  //     newHeight += 10;
+  //   }
+
+  //   this.book1.style.top = newTop + 'px';
+  //   this.book1.style.left = newLeft + 'px';
+  //   this.book1.style.width = newWidth + 'px';
+  //   this.book1.style.height = newHeight + 'px';
+  // }
 }
