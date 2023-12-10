86d261e62c83a79cf199469350ba1fac9b6e0466
Ayad <ayadjacob7@gmail.com>
2023-12-10 20:32:53 +0200

    Update-26

--- src/app/components/historical-readers/historical-readers.component.ts
+++ src/app/components/historical-readers/historical-readers.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  Renderer2,
-  AfterViewInit,
-} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-historical-readers',
@@ -20,13 +14,76 @@ export class HistoricalReadersComponent {
   isZooming_hatchepsuit: boolean = false;
   isScaling_hatchepsuit: boolean = false;
 
+  isBouncing_crazy: boolean = false;
+  isZooming_crazy: boolean = false;
+  isScaling_crazy: boolean = false;
+
+  isBouncing_genghis: boolean = false;
+  isZooming_genghis: boolean = false;
+  isScaling_genghis: boolean = false;
+
+  isBouncing_fransisco: boolean = false;
+  isZooming_fransisco: boolean = false;
+  isScaling_fransisco: boolean = false;
+
+  isBouncing_hungwu: boolean = false;
+  isZooming_hungwu: boolean = false;
+  isScaling_hungwu: boolean = false;
+
+  isBouncing_freedom: boolean = false;
+  isZooming_freedom: boolean = false;
+  isScaling_freedom: boolean = false;
+
+  isBouncing_catherine: boolean = false;
+  isZooming_catherine: boolean = false;
+  isScaling_catherine: boolean = false;
+
+  isBouncing_queen: boolean = false;
+  isZooming_queen: boolean = false;
+  isScaling_queen: boolean = false;
+
   ngOnInit(): void {
-    this.cabeza();
+    this.crazy();
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.cabeza();
+    }, 1000); // Adjust the timeout value based on your animation duration
 
     setTimeout(() => {
       // Navigate to the specified route
       this.hatchepsuit();
-    }, 1000); // Adjust the timeout value based on your animation duration
+    }, 2000); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.genghis();
+    }, 3000); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.fransisco();
+    }, 4000); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.hungwu();
+    }, 5000); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.freedom();
+    }, 6000); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.catherine();
+    }, 7000); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.queen();
+    }, 8000); // Adjust the timeout value based on your animation duration
   }
 
   cabeza(): void {
@@ -60,6 +117,125 @@ export class HistoricalReadersComponent {
     setTimeout(() => {
       // Navigate to the specified route
       this.isZooming_hatchepsuit = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  crazy(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_crazy = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_crazy = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_crazy = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  genghis(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_genghis = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_genghis = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_genghis = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  fransisco(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_fransisco = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_fransisco = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_fransisco = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  hungwu(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_hungwu = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_hungwu = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_hungwu = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  freedom(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_freedom = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_freedom = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_freedom = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  catherine(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_catherine = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_catherine = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_catherine = true;
+    }, 2000); // Adjust the timeout value based on your animation duration
+  }
+
+  queen(): void {
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isScaling_queen = true;
+    }, 600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isBouncing_queen = true;
+    }, 1600); // Adjust the timeout value based on your animation duration
+
+    setTimeout(() => {
+      // Navigate to the specified route
+      this.isZooming_queen = true;
     }, 2000); // Adjust the timeout value based on your animation duration
   }
 }
